0bd64a68b6e4028b391c73183f28ad6ba333631d
Amir B. Kahna <amir.kahna2@gmail.com>
2026-03-20 14:59:45 +0100

    Adding JS file

---
+++ JS/D10_no-date.js
@@ -0,0 +1,536 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+// JS Basics
+
+/* ESERCIZIO A
+  Crea una variabile chiamata "sum" e assegnaci il risultato della somma tra i valori 10 e 20.
+*/
+console.log("Exercise A");
+const sum = 10 + 20;
+console.log(sum);
+
+/* ESERCIZIO B
+  Crea una variabile chiamata "random" e assegnaci un numero casuale tra 0 e 20 (deve essere generato dinamicamente a ogni esecuzione).
+*/
+console.log("Exercise B");
+const random = Math.floor(Math.random() * 21);
+console.log(random);
+
+/* ESERCIZIO C
+  Crea una variabile chiamata "me" e assegnaci un oggetto contenente le seguenti proprietà: name = il tuo nome, surname = il tuo cognome, age = la tua età.
+*/
+console.log("Exercise C");
+const me = {
+  firstName: "Amir",
+  lastName: "Kahna",
+  age: "26",
+};
+console.log(me);
+
+/* ESERCIZIO D
+  Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
+*/
+console.log("Exercise D");
+delete me.age;
+console.log(me);
+
+/* ESERCIZIO E
+  Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
+*/
+console.log("Exercise E");
+me.skills = ["JavaScript", "HTML", "CSS"];
+console.log(me.skills);
+
+/* ESERCIZIO F
+  Crea un pezzo di codice per aggiungere un nuovo elemento all'array "skills" contenuto nell'oggetto "me".
+*/
+console.log("Exercise F");
+me.skills.push("CSS");
+console.log(me.skills);
+
+/* ESERCIZIO G
+  Crea un pezzo di codice per rimuovere programmaticamente l'ultimo elemento dall'array "skills" contenuto nell'oggetto "me".
+*/
+console.log("Exercise G");
+me.skills.pop();
+console.log(me.skills);
+
+// Funzioni
+
+/* ESERCIZIO 1
+  Crea una funzione chiamata "dice": deve generare un numero casuale tra 1 e 6.
+*/
+console.log("Exercise 1");
+const dice = () => {
+  const roll = Math.ceil(Math.random() * 6);
+  return roll;
+};
+console.log(dice());
+
+/* ESERCIZIO 2
+  Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
+*/
+console.log("Exercise 2");
+const whoIsBigger = (n1, n2) => {
+  if (n1 < n2) {
+    return n2;
+  } else {
+    return n1;
+  }
+};
+console.log(whoIsBigger(10, 2));
+
+/* ESERCIZIO 3
+  Crea una funzione chiamata "splitMe" che riceve una stringa come parametro e ritorna un'array contenente ogni parola della stringa.
+
+  Es.: splitMe("I love coding") => ritorna ["I", "Love", "Coding"]
+*/
+console.log("Exercise 3");
+const splitMe = (str) => {
+  const split = str.split(" ");
+  return split;
+};
+console.log(splitMe("Hello beautiful people"));
+
+/* ESERCIZIO 4
+  Crea una funzione chiamata "deleteOne" che riceve una stringa e un booleano come parametri.
+  Se il valore booleano è true la funzione deve ritornare la stringa senza il primo carattere, altrimenti la deve ritornare senza l'ultimo.
+*/
+console.log("Exercise 4");
+const deleteOne = (str, n) => {
+  if (n === true) {
+    return str.slice(1);
+  } else {
+    return str.slice(0, -1);
+  }
+};
+console.log(deleteOne("Hello", true));
+/* ESERCIZIO 5
+  Crea una funzione chiamata "onlyLetters" che riceve una stringa come parametro e la ritorna eliminando tutte le cifre numeriche.
+
+  Es.: onlyLetters("I have 4 dogs") => ritorna "I have dogs"
+*/
+console.log("Exercise 5");
+
+// const onlyLetters = (str) => {
+//   let text = "";
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] < "0" || str[i] > "9") {
+//       text += str[i];
+//     }
+//   }
+//   return textJoin;
+// };
+// console.log(onlyLetters("Hello beautiful people 210"));
+
+const onlyLetters = (str) => {
+  return str.replace(/[0-9]/g, "");
+};
+console.log(onlyLetters("Hello beautiful people 20384"));
+
+/* ESERCIZIO 6
+  Crea una funzione chiamata "isThisAnEmail" che riceve una stringa come parametro e ritorna true se la stringa è un valido indirizzo email.
+*/
+console.log("Exercise 6");
+const isThisAnEmail = (str) => {
+  if (str.includes("@") && str.includes(".")) {
+    return true;
+  }
+  {
+    return false;
+  }
+};
+console.log(isThisAnEmail("test@gmail.com"));
+/* ESERCIZIO 8
+  Scrivi una funzione chiamata "rollDice" che riceve un numero come parametro.
+  Deve invocare la precedente funzione dice() il numero di volte specificato nel parametro, e deve tornare un oggetto contenente una proprietà "sum":
+  il suo valore deve rappresentare il totale di tutti i valori estratti con le invocazioni di dice().
+  L'oggetto ritornato deve anche contenere una proprietà "values", contenente un array con tutti i valori estratti dalle invocazioni di dice().
+
+  Example:
+  rollDice(3) => ritorna {
+      sum: 10
+      values: [3, 3, 4]
+  }
+*/
+console.log("Exercise 8");
+const rollDice = (n) => {
+  let sum = 0;
+  let values = [];
+  for (let i = 0; i < n; i++) {
+    let roll = dice();
+    sum += roll;
+    values.push(roll);
+  }
+  return {
+    sum: sum,
+    values: values,
+  };
+};
+console.log(rollDice(3));
+
+// Arrays & Oggetti
+
+// NOTA: l'array "movies" usato in alcuni esercizi è definito alla fine di questo file
+
+const movies = [
+  {
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+    Year: "2001",
+    imdbID: "tt0120737",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+  },
+
+  {
+    Title: "The Lord of the Rings: The Return of the King",
+    Year: "2003",
+    imdbID: "tt0167260",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lord of the Rings: The Two Towers",
+    Year: "2002",
+    imdbID: "tt0167261",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of War",
+    Year: "2005",
+    imdbID: "tt0399295",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lords of Dogtown",
+    Year: "2005",
+    imdbID: "tt0355702",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lord of the Rings",
+    Year: "1978",
+    imdbID: "tt0077869",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of the Flies",
+    Year: "1990",
+    imdbID: "tt0100054",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lords of Salem",
+    Year: "2012",
+    imdbID: "tt1731697",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+    Year: "1984",
+    imdbID: "tt0087365",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of the Flies",
+    Year: "1963",
+    imdbID: "tt0057261",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Avengers",
+    Year: "2012",
+    imdbID: "tt0848228",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Infinity War",
+    Year: "2018",
+    imdbID: "tt4154756",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Age of Ultron",
+    Year: "2015",
+    imdbID: "tt2395427",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Endgame",
+    Year: "2019",
+    imdbID: "tt4154796",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+  },
+];
+
+/* ESERCIZIO 11
+  Scrivi una funzione chiamata "deleteProp" che riceve un oggetto e una stringa come parametri; deve ritornare l'oggetto fornito dopo aver eliminato
+  in esso la proprietà chiamata come la stringa passata come secondo parametro.
+*/
+console.log("Exercise 11");
+const props = {
+  prop1: "Cup",
+  prop2: "Card",
+  prop3: "sword",
+};
+const deleteProp = (obj, str) => {
+  delete obj[str];
+  return obj;
+};
+console.log(deleteProp(props, "prop2"));
+
+/* ESERCIZIO 12
+  Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
+*/
+console.log("Exercise 12");
+let newestMovie = 0;
+const newest = () => {
+  movies.forEach((n) => {
+    if (n.Year > newestMovie) {
+      newestMovie = n;
+    }
+  });
+};
+newest();
+console.log(newestMovie);
+
+/* ESERCIZIO 13
+  Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
+*/
+console.log("Exercise 13");
+const countMovies = () => {
+  return movies.length;
+};
+console.log(countMovies());
+
+/* ESERCIZIO 14
+  Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
+*/
+console.log("Exercise 14");
+const onlyTheYears = () => {
+  let years = [];
+  for (let i = 0; i < movies.length; i++) {
+    years.push(parseInt(movies[i].Year));
+  }
+  return years;
+};
+console.log(onlyTheYears());
+
+/* ESERCIZIO 15
+  Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
+*/
+console.log("Exercise 15");
+const onlyInLastMillennium = () => {
+  let oldMovies = [];
+  for (let i = 0; i < movies.length; i++) {
+    if (parseInt(movies[i].Year) < 2000) {
+      oldMovies.push(movies[i]);
+    }
+  }
+  return oldMovies;
+};
+console.log(onlyInLastMillennium());
+
+/* ESERCIZIO 16
+  Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
+*/
+console.log("Exercise 16");
+const sumAllTheYears = (movies) => {
+  return movies.reduce((acc, film) => {
+    return acc + parseInt(film.Year);
+  }, 0);
+};
+console.log(sumAllTheYears(movies));
+
+/* ESERCIZIO 17
+  Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
+*/
+console.log("Exercise 17");
+
+const searchByTitle = (str) => {
+  return movies.filter((movie) =>
+    movie.Title.toLowerCase().includes(str.toLowerCase()),
+  );
+};
+console.log(searchByTitle("Lord of the rings"));
+
+/* ESERCIZIO 18
+  Scrivi una funzione chiamata "searchAndDivide" che riceve una stringa come parametro e ritorna un oggetto contenente due array: "match" e "unmatch".
+  "match" deve includere tutti i film dell'array "movies" fornito che contengono la stringa fornita all'interno del proprio titolo, mentre "unmatch" deve includere tutti i rimanenti.
+*/
+console.log("Exercise 18");
+const searchAndDivide = (str) => {
+  const match = movies.filter((movie) =>
+    movie.Title.toLowerCase().includes(str.toLowerCase()),
+  );
+
+  const unmatch = movies.filter(
+    (movie) => !movie.Title.toLowerCase().includes(str.toLowerCase()),
+  );
+
+  return { match, unmatch };
+};
+
+console.log(searchAndDivide("Lord of the Rings"));
+
+/* ESERCIZIO 19
+  Scrivi una funzione chiamata "removeIndex" che riceve un numero come parametro e ritorna l'array "movies" fornito privo dell'elemento nella posizione ricevuta come parametro.
+*/
+
+// DOM (nota: gli elementi che selezionerai non si trovano realmente nella pagina)
+console.log("Exercise 19");
+const removeIndex = (n) => {
+  movies.splice(n - 1, 1);
+  return movies;
+};
+console.log(removeIndex(3));
+/* ESERCIZIO 20
+  Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
+*/
+console.log("Exercise 20");
+const container = () => {
+  const container = document.getElementById("container");
+  return container;
+};
+console.log(container());
+/* ESERCIZIO 21
+  Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
+*/
+console.log("Exercise 21");
+const tags = () => {
+  const tags = document.querySelectorAll("td");
+  return tags;
+};
+console.log(tags());
+/* ESERCIZIO 22
+  Scrivi una funzione che, tramite un ciclo, stampa in console il testo contenuto in ogni tag <td> all'interno della pagina.
+*/
+console.log("Exercise 22");
+const loopTd = () => {
+  const innerT = document.querySelectorAll("td");
+  for (let i = 0; i < innerT.length; i++) {
+    console.log(innerT[i].textContent);
+  }
+  return innerT;
+};
+console.log(loopTd());
+/* ESERCIZIO 23
+  Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
+*/
+console.log("Exercise 23");
+const linkBackground = () => {
+  const linkB = document.querySelectorAll("a");
+  linkB.forEach((link) => {
+    link.style.backgroundColor = "red";
+  });
+};
+linkBackground();
+/* ESERCIZIO 24
+  Scrivi una funzione per aggiungere un nuovo elemento alla lista non ordinata con id "myList".
+*/
+console.log("Excerise 24");
+const li = (str) => {
+  const liFirst = document.createElement("li");
+  liFirst.innerText = str;
+  myList.appendChild(liFirst);
+};
+li(5);
+
+/* ESERCIZIO 25
+  Scrivi una funzione per svuotare la lista non ordinata con id "myList".
+*/
+console.log("Exercise 25");
+// const unLi = () => {
+//   return myList.remove();
+// };
+// unLi();
+
+const unLi = () => {
+  const myList = document.getElementById("myList");
+  myList.innerHTML = "";
+};
+unLi();
+
+/* ESERCIZIO 26
+  Scrivi una funzione per aggiungere ad ogni tag <tr> la classe CSS "test"
+*/
+console.log("Exercise 26");
+const classTr = () => {
+  const classTr = document.querySelectorAll("tr");
+  classTr.forEach((tr) => {
+    tr.classList.add("test");
+  });
+};
+classTr();
+
+// [EXTRA] JS Avanzato
+
+/* ESERCIZIO 27
+  Crea una funzione chiamata "halfTree" che riceve un numero come parametro e costruisce un mezzo albero di "*" (asterischi) dell'altezza fornita.
+
+  Esempio:
+  halfTree(3)
+
+  *
+  **
+  ***
+
+*/
+console.log("Exercise 27");
+const halfTree = (n) => {
+  for (let i = 0; i < n; i++) {
+    console.log("*".repeat(i));
+  }
+};
+halfTree(5);
+
+/* ESERCIZIO 28
+  Crea una funzione chiamata "tree" che riceve un numero come parametro e costruisce un albero di "*" (asterischi) dell'altezza fornita.
+
+  Esempio:
+  tree(3)
+
+    *
+   ***
+  *****
+
+*/
+
+/* ESERCIZIO 29
+  Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
+*/
+
+/* Questo array viene usato per gli esercizi. Non modificarlo. */
